72f6fcc47fb7c69339a75cd24183206cdfefc032
zhaoguangwei <zhaoguangwei@jd.com>
2020-04-18 14:17:40 +0800

    优化权限验证代码;

--- contract-starter/src/test/java/com/jd/blockchain/PermissionTest.java
+++ contract-starter/src/test/java/com/jd/blockchain/PermissionTest.java
@@ -28,102 +28,33 @@ public class PermissionTest extends SDK_Base_Demo {
     }
 
     /**
-     * 新增加一个角色
+     * 新增加角色
      */
     @Test
-    public void executeRoleConfig_cannot_registerUser() {
+    public void executeRoleConfig_moreRoles() {
         // 定义交易模板
-        TransactionTemplate txTpl = blockchainService.newTransaction(ledgerHash);
-        // 定义角色权限；
-        txTpl.security().roles().configure("NORMAL")
-                .enable(LedgerPermission.REGISTER_DATA_ACCOUNT)
-                .disable(LedgerPermission.REGISTER_USER)
-                .enable(TransactionPermission.DIRECT_OPERATION)
-                .enable(TransactionPermission.CONTRACT_OPERATION);
-
-        TransactionResponse txResp = commit(txTpl);
-        System.out.println(txResp.isSuccess());
-    }
-
-    /**
-     * admin赋权给newUser,can register user;
-     */
-    @Test
-    public void registerUser_cannotRegisterUser() {
-        //add the role:MANAGERX
-        executeRoleConfig_cannot_registerUser();
-
-        // 在本地定义注册账号的 TX；
         TransactionTemplate txTemp = blockchainService.newTransaction(ledgerHash);
-        BlockchainKeypair newUser = registerUser();
-
-//        txTemp.security().authorziations().forUser(newUser.getAddress()).authorize("MANAGER2").setPolicy(RolesPolicy.UNION).authorize("MANAGER2");
-        txTemp.security().authorziations().forUser(newUser.getAddress()).unauthorize("DEFAULT").authorize("NORMAL");
-
-        // TX 准备就绪；
-        PreparedTransaction prepTx = txTemp.prepare();
-        prepTx.sign(adminKey);
-
-        // 提交交易；
-        TransactionResponse response = prepTx.commit();
-        Assert.assertTrue(response.isSuccess());
-
-        //then sign by newUser;
-        // 在本地定义注册账号的 TX；
-        registerUserByNewSigner(newUser);
-    }
-
-    /**
-     * 新增加一个角色
-     */
-    @Test
-    public void executeRoleConfig_manager2() {
-        // 定义交易模板
-        TransactionTemplate txTpl = blockchainService.newTransaction(ledgerHash);
         // 定义角色权限；
-        txTpl.security().roles().configure("MANAGER2")
-                .enable(LedgerPermission.REGISTER_USER)
+        txTemp.security().roles().configure("MANAGER0")
                 .enable(LedgerPermission.CONFIGURE_ROLES)
+                .enable(TransactionPermission.DIRECT_OPERATION);
+        txTemp.security().roles().configure("MANAGER1")
+                .enable(LedgerPermission.REGISTER_USER)
+                .enable(TransactionPermission.DIRECT_OPERATION);
+        txTemp.security().roles().configure("MANAGER2")
+                .enable(LedgerPermission.REGISTER_DATA_ACCOUNT)
+                .enable(TransactionPermission.DIRECT_OPERATION);
+        txTemp.security().roles().configure("MANAGER3")
                 .enable(LedgerPermission.REGISTER_DATA_ACCOUNT)
                 .enable(LedgerPermission.WRITE_DATA_ACCOUNT)
-//                .disable(LedgerPermission.REGISTER_USER)
                 .enable(TransactionPermission.DIRECT_OPERATION);
 
-        TransactionResponse txResp = commit(txTpl);
+        TransactionResponse txResp = commit(txTemp);
         System.out.println(txResp.isSuccess());
     }
 
     /**
-     * admin赋权给newUser,can register user;
-     */
-    @Test
-    public void registerUser_canRegisterUser() {
-        // 在本地定义注册账号的 TX；
-        TransactionTemplate txTemp = blockchainService.newTransaction(ledgerHash);
-        BlockchainKeypair newUser = registerUser();
-
-        // 定义角色权限；
-        txTemp.security().roles().configure("MANAGER1")
-                .enable(LedgerPermission.REGISTER_USER, LedgerPermission.REGISTER_DATA_ACCOUNT)
-                .enable(TransactionPermission.CONTRACT_OPERATION);
-        txTemp.security().authorziations().forUser(newUser.getIdentity()).authorize("MANAGER1");
-
-        // TX 准备就绪；
-        PreparedTransaction prepTx = txTemp.prepare();
-        prepTx.sign(adminKey);
-
-        // 提交交易；
-        prepTx.commit();
-
-        //then sign by newUser;
-        // 在本地定义注册账号的 TX；
-        registerUserByNewSigner(newUser);
-    }
-
-
-
-    /**
-     * 针对系统中已经注册的用户进行赋权;
+     * 针对系统中已经注册的用户赋予角色;
      */
     @Test
     public void addPermission4ExistUser() {
@@ -137,27 +68,19 @@ public class PermissionTest extends SDK_Base_Demo {
          * pass=abc
          */
 
-        PrivKey privKey = KeyGenUtils.decodePrivKeyWithRawPassword("177gjyztVu92xSMda4FkhHfS6CvisvJ4nC9mSVscVsvAWN649Epy6yZ1PYYTZ4vaG1ByWZA",
-                "abc");
+        PrivKey privKey = KeyGenUtils.decodePrivKey("177gjyztVu92xSMda4FkhHfS6CvisvJ4nC9mSVscVsvAWN649Epy6yZ1PYYTZ4vaG1ByWZA",
+                "DYu3G8aGTMBW1WrTw76zxQJQU4DHLw9MLyy7peG4LKkY");
         PubKey pubKey = KeyGenUtils.decodePubKey("3snPdw7i7PjXU3qkPdRNRch974TDGbqim2Dm1GbJDuUYqfjyYUEfSU");
         BlockchainKeypair newUser = new BlockchainKeypair(pubKey, privKey);
         System.out.println("user'id="+newUser.getAddress());
         System.out.println("pubKey="+newUser.getPubKey().toBase58());
         System.out.println("privKey="+newUser.getPrivKey().toBase58());
 
-        txTemp.users().register(newUser.getIdentity());
+//        txTemp.users().register(newUser.getIdentity());
 
-        // 定义角色权限；
-        txTemp.security().roles().configure("MANAGER")
-                .enable(LedgerPermission.REGISTER_USER, LedgerPermission.REGISTER_DATA_ACCOUNT)
-                .enable(TransactionPermission.CONTRACT_OPERATION);
-        txTemp.security().authorziations().forUser(newUser.getIdentity()).authorize("MANAGER");
-
-        // TX 准备就绪；
-        PreparedTransaction prepTx = txTemp.prepare();
-        prepTx.sign(adminKey);
-
-        // 提交交易；
-        prepTx.commit();
+        txTemp.security().authorziations().forUser(newUser.getIdentity()).
+                setPolicy(RolesPolicy.UNION).authorize("MANAGER0").
+                unauthorize("MANAGER1").authorize("MANAGER3");
+        commitA(txTemp,adminKey);
     }
 }
