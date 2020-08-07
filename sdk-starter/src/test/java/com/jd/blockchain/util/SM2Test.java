59a8285d5d06dacc773898d3fc8cc31e284d047f
zhaoguangwei <zhaoguangwei@jd.com>
2020-08-07 16:57:53 +0800

    use: BlockchainKeyGenerator.getInstance().generate("SM2");

--- sdk-starter/src/test/java/com/jd/blockchain/util/SM2Test.java
+++ sdk-starter/src/test/java/com/jd/blockchain/util/SM2Test.java
@@ -2,6 +2,8 @@ package com.jd.blockchain.util;
 
 import com.jd.blockchain.crypto.AsymmetricKeypair;
 import com.jd.blockchain.crypto.Crypto;
+import com.jd.blockchain.ledger.BlockchainKeyGenerator;
+import com.jd.blockchain.ledger.BlockchainKeypair;
 import com.jd.blockchain.utils.codec.Base58Utils;
 import com.jd.blockchain.utils.io.ByteArray;
 import com.jd.blockchain.utils.security.ShaUtils;
@@ -28,10 +30,14 @@ public class SM2Test {
      * 生成密钥，要求输入密码用于保护私钥文件；
      */
     private void generateKeyPair() {
+        //use generate;
+        BlockchainKeypair blockchainKeypair = BlockchainKeyGenerator.getInstance().generate("SM2");
+        //register user by blockchainKeypair.getIdentity();
+        logger.info("generate by SM2, address={}",blockchainKeypair.getIdentity().getAddress());
+
+        //use another method;
         AsymmetricKeypair kp = Crypto.getSignatureFunction("SM2").generateKeypair();
-
         String base58PubKey = encodePubKey(kp.getPubKey());
-
         String pwd="abc";
         byte[] pwdBytes = ShaUtils.hash_256(ByteArray.fromString(pwd, "UTF-8"));
         String base58PrivKey = encodePrivKey(kp.getPrivKey(), pwdBytes);
